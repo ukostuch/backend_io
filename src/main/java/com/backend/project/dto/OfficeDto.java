c10189a0974e03309b9f6ef98471d9174056b68c
Wojciech Labedz <wojciech.labedz2@gmail.com>
2025-04-23 19:59:23 +0200

    new pictures, new offices, new FAQs, validation of +48 phone numbers

--- src/main/java/com/backend/project/dto/OfficeDto.java
+++ src/main/java/com/backend/project/dto/OfficeDto.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.*;
 
 public record OfficeDto(
         @NotNull @Valid DistrictDto district,
-        @NotBlank @Pattern(regexp = "^(?:\\+43|0)(?:\\s?\\d{1,4})(?:\\s?\\d{1,4})(?:\\s?\\d{4})$", message = "Invalid phone number format") String phoneNumber,
+        @NotBlank @Pattern(regexp = "^(?:\\+48|48|0)?\\s?\\d{3}[\\s-]?\\d{3}[\\s-]?\\d{3}$", message = "Invalid phone number format") String phoneNumber,
         @NotBlank @Size(max = 500, message = "Description cannot exceed 500 characters") String description,
         @NotBlank String address
 ) {}
--- src/main/java/com/backend/project/dto/OfficeRetDto.java
+++ src/main/java/com/backend/project/dto/OfficeRetDto.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 public record OfficeRetDto(
         @NotNull UUID id,
         @NotNull @Valid DistrictDto district,
-        @NotBlank @Pattern(regexp = "^(?:\\+43|0)(?:\\s?\\d{1,4})(?:\\s?\\d{1,4})(?:\\s?\\d{4})$", message = "Invalid phone number format") String phoneNumber,
+        @NotBlank @Pattern(regexp = "^(?:\\+48|48|0)?\\s?\\d{3}[\\s-]?\\d{3}[\\s-]?\\d{3}$", message = "Invalid phone number format") String phoneNumber,
         @NotBlank String address,
         @NotBlank String photo,
         @NotBlank @Size(max = 300, message = "Description cannot exceed 300 characters") String description
